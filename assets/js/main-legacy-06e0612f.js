1279491d1824a4878b7fa6868d5faa13e3833bfb
exslym <76844562+exslym@users.noreply.github.com>
2023-03-30 18:17:51 +0400

    Updates

---
+++ assets/js/main-legacy-06e0612f.js
@@ -0,0 +1,1 @@
+System.register(["./index-legacy-7d4dea58.js"],(function(e,t){"use strict";return{setters:[null],execute:function(){}}}));
--- assets/js/main-legacy-3abb5437.js
+++
@@ -1,1 +0,0 @@
-System.register(["./index-legacy-3fca14df.js"],(function(e,t){"use strict";return{setters:[null],execute:function(){}}}));
