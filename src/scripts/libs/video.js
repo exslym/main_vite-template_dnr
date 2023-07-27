0e17443882d6833b667c9f7d6f64d9134e978ed5
exslym <76844562+exslym@users.noreply.github.com>
2023-07-27 21:01:30 +0400

    video lib updated

--- src/scripts/libs/video.js
+++ src/scripts/libs/video.js
@@ -3,22 +3,23 @@ export function videoPlayer(startButton, startedClass) {
     const videoPosters = document.querySelectorAll(`.${startButton}`);
     const videos = document.querySelectorAll('video');
 
-    let Play = (videoId) => {
+    let videoPlay = (videoId) => {
       for (let poster of videoPosters) {
-        if (poster.dataset.label === videoId) {
+        if (poster.dataset.target === videoId) {
           poster.classList.add(`${startedClass}`);
         }
       }
       for (let video of videos) {
-        if (video.dataset.label === videoId) {
+        if (video.dataset.target === videoId) {
           video.play();
+          video.setAttribute('controls', 'true');
         }
       }
     };
 
-    let Stop = (videoId) => {
+    let videoStop = (videoId) => {
       for (let video of videos) {
-        if (video.dataset.label === videoId) {
+        if (video.dataset.target === videoId) {
           video.pause();
         }
       }
@@ -27,8 +28,8 @@ export function videoPlayer(startButton, startedClass) {
     for (let poster of videoPosters) {
       poster.addEventListener('click', (e) => {
         e.preventDefault();
-        let videoId = poster.dataset.label;
-        Play(videoId);
+        let videoId = poster.dataset.target;
+        videoPlay(videoId);
       });
     }
 
@@ -44,3 +45,60 @@ export function videoPlayer(startButton, startedClass) {
     }
   }
 }
+
+//! html structure - copy templates to your document:
+//* VIDEO:
+/* 
+  <div class="videobox">
+    <video preload="auto" data-label="Video" data-target="video1" id="video1">
+      <source src="video/video.mp4" type="video/mp4" />
+    </video>
+    <div class="startButton" data-target="video1"></div>
+  </div>
+*/
+
+//! styles structure - copy _modals.scss module to your project and import it in the index.scss
+/* 
+  .videobox {
+    position: relative;
+    width: 100%;
+    aspect-ratio: 1.78;
+    @include adapt(border-radius, 20, 40);
+    video {
+      cursor: pointer;
+      margin: 0;
+      padding: 0;
+      display: block;
+      @include adapt(border-radius, 20, 40);
+      width: 100%;
+      height: 100%;
+    }
+    .startButton {
+      cursor: pointer;
+      margin: 0;
+      padding: 0;
+      position: absolute;
+      left: 0;
+      top: 0;
+      width: 100%;
+      height: 100%;
+      background: no-repeat center / cover url('../assets/images/poster.png');
+      @include adapt(border-radius, 20, 40);
+    }
+    .startedClass {
+      display: none;
+    }
+  }
+*/
+
+//! scripts - add module to your project and import it in the index.js
+//* SCRIPTS:
+/*
+import { videoPlayer } from './libs/video';
+
+window.addEventListener('DOMContentLoaded', () => {
+  'use strict';
+
+  videoPlayer('startButton', 'startedClass');
+});
+*/
