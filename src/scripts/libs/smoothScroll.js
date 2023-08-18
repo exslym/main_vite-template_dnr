c5e51fc8385c5bef8eaec5bcfa701118286e0da3
exslym <76844562+exslym@users.noreply.github.com>
2023-08-18 18:19:31 +0400

    lib updated: smoothScroll, new lib added: videoTimeCodes

--- src/scripts/libs/smoothScroll.js
+++ src/scripts/libs/smoothScroll.js
@@ -5,22 +5,40 @@ export function smoothScroll(anchorClass) {
       link.addEventListener('click', (e) => {
         switch (true) {
           //* for tags e.g. <a href="#block"></a>
-          case link.hasAttribute('href'):
-            if (document.getElementById(link.getAttribute('href').substring(1))) {
+          case link.hasAttribute('href') && !link.hasAttribute('data-scroll'):
+            if (document.querySelector(`#${link.getAttribute('href').substring(1)}`)) {
               e.preventDefault();
               const href = link.getAttribute('href').substring(1);
-              const scrollTarget = document.getElementById(href);
-              scrollToTaarget(scrollTarget);
+              const scrollTarget = document.querySelector(`#${href}`);
+              scrollToTarget(scrollTarget);
+            }
+            break;
+          //* for tags e.g. <a href="#block" data-scroll="scrollTo"></a>
+          case link.hasAttribute('href') && link.hasAttribute('data-scroll'):
+            if (document.querySelector(`.${link.dataset.scroll}`)) {
+              e.preventDefault();
+              const targetElement = link.dataset.scroll;
+              const scrollTarget = document.querySelector(`.${targetElement}`);
+              scrollToTarget(scrollTarget);
             }
             break;
 
           //* for tags e.g. <button data-target="element"></button>
-          case link.hasAttribute('data-target'):
-            if (document.getElementById(link.dataset.target)) {
+          case link.hasAttribute('data-target') && !link.hasAttribute('data-scroll'):
+            if (document.querySelector(`#${link.dataset.target}`)) {
               e.preventDefault();
               const targetElement = link.dataset.target;
-              const scrollTarget = document.getElementById(targetElement);
-              scrollToTaarget(scrollTarget);
+              const scrollTarget = document.querySelector(`#${targetElement}`);
+              scrollToTarget(scrollTarget);
+            }
+            break;
+          //* for tags e.g. <button data-target="element" data-scroll="scrollTo"></button>
+          case link.hasAttribute('data-target') && link.hasAttribute('data-scroll'):
+            if (document.querySelector(`.${link.dataset.scroll}`)) {
+              e.preventDefault();
+              const targetElement = link.dataset.scroll;
+              const scrollTarget = document.querySelector(`.${targetElement}`);
+              scrollToTarget(scrollTarget);
             }
             break;
         }
@@ -29,13 +47,8 @@ export function smoothScroll(anchorClass) {
   }
 }
 
-function scrollToTaarget(scrollTarget) {
-  //! если не нужен отступ сверху:
-  // const topOffset = 0;
-  //! если есть fixed:
-  // document.querySelector('.scrollto').offsetHeight;
-
-  const topOffset = 0;
+function scrollToTarget(scrollTarget) {
+  const topOffset = 20;
   const elementPosition = scrollTarget.getBoundingClientRect().top;
   const offsetPosition = elementPosition - topOffset;
 
@@ -44,3 +57,40 @@ function scrollToTaarget(scrollTarget) {
     behavior: 'smooth',
   });
 }
+
+//! html structure - copy templates to your document:
+//* LINKS:
+/* 
+  <a
+    href="#element"
+    class="scrollButton"
+    data-scroll="scrollTo"
+  >
+    Link to element
+  </a>
+
+  <button
+    class="scrollButton"
+    data-target="element"
+    data-scroll="scrollTo"
+  >
+    Link to element
+  </button>
+  ---
+  ---
+  <div class="scrollTo" id="element">
+    element
+  </div>
+*/
+
+//! scripts - add module to your project and import it in the index.js
+//* SCRIPTS:
+/*
+import { smoothScroll } from './libs/smoothScroll';
+
+window.addEventListener('DOMContentLoaded', () => {
+  'use strict';
+
+  smoothScroll('scrollButton');
+});
+*/
