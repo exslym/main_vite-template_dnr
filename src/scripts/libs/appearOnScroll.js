9a5287b0e52b4fa76136bd994d1d2cc2bdc369a5
exslym <76844562+exslym@users.noreply.github.com>
2023-08-29 17:34:26 +0400

    appearsOnScroll.js updated

--- src/scripts/libs/appearOnScroll.js
+++ src/scripts/libs/appearOnScroll.js
@@ -1,15 +1,20 @@
 //* ELEMENT APPEARS ON SCROLL
 export function appearOnScroll(elementClass, activeClass) {
-  const h = document.documentElement;
+  const d = document.documentElement;
   const b = document.body;
   const st = 'scrollTop';
   const sh = 'scrollHeight';
+  const width = window.innerWidth || d.clientWidth || b.clientWidth;
+
   const upButton = document.querySelector(`.${elementClass}`);
 
   if (document.querySelector(`.${elementClass}`)) {
     document.addEventListener('scroll', () => {
-      let scrollTop = h[st] || b[st];
-      let percent = ((h[st] || b[st]) / ((h[sh] || b[sh]) - h.clientHeight)) * 100;
+      let percent = ((d[st] || b[st]) / ((d[sh] || b[sh]) - d.clientHeight)) * 100;
+
+      if (width > 600) {
+        // if depence of width is needed
+      }
 
       if (percent > 10) {
         upButton.classList.add(`${activeClass}`);
