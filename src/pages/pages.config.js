902a4299e3ef1e0244dda79db68e5a92c5f56c4e
exslym <76844562+exslym@users.noreply.github.com>
2024-05-23 13:37:07 +0300

    structure fixed

--- src/pages/pages.config.js
+++
@@ -1,9 +0,0 @@
-import { resolve } from 'path';
-
-const pages = [
-  { name: 'main', path: resolve(__dirname, '../index.html') },
-  { name: 'page1', path: resolve(__dirname, '../page1.html') }
-  // { name: 'page2', path: resolve(__dirname, '../page2.html') },
-];
-
-export default pages;
